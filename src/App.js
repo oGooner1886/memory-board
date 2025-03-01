d3f252147cfb03d51100ea821b2f26c8136b29f0
unknown <MyNameIsMyth>
2025-03-01 16:19:44 +0300

    photo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import img from './img/main.png';
+import img1 from './img/Аджимуратов.png';
+import img2 from './img/Бабенко.png';
+import img3 from './img/Мешечко.png';
+import img4 from './img/Уваров.png';
+import img5 from './img/Черкасов.png';
 import './App.css';
 
-function App() {
+const App = () => {
+  const images = [img1, img2, img3, img4, img5];
+  const names = [
+    { id: 1, fullName: 'Аджимуратов Зеит Агезекович' },
+    { id: 2, fullName: 'Бабенко Алексей Андреевич' },
+    { id: 3, fullName: 'Мешечко Терентий Иванович' },
+    { id: 4, fullName: 'Уваров Иван Васильвевич' },
+    { id: 5, fullName: 'Черкасов Алексей Андреевич' },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="img-container">
+        <img className="imgmain" src={img} alt="Top" />
+      </div>
+      <div className="text-container">
+        <p id="save">//Сохраним подвиг в памяти</p>
+        <h1 className="history">Истории героев</h1>
+        <hr />
+      </div>
+      <div className="image-gallery">
+        {images.map((image, index) => (
+          <div key={index} className="image-item">
+            <div className="name-item">
+              <p>{names[index].id}. {names[index].fullName}</p>
+            </div>
+            <img src={image} alt={`Image ${index + 1}`} style={{ width: '356px', margin: '10px' }} />
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
